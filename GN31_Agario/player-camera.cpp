ca1a5ebc577d4d8afb2f3d9c89871ed174034bd4
i-yuuki <i-yuuki@users.noreply.github.com>
2022-01-17 15:10:07 +0900

    カメラ位置の符号をなんとなく調整
    カメラ位置と追従するプレイヤーの位置が同じなほうが
    カメラ感あっていいなって

--- GN31_Agario/player-camera.cpp
+++ GN31_Agario/player-camera.cpp
@@ -5,7 +5,7 @@ void PlayerCamera::update(float deltaTime){
   Near::Math::Vector3 targetPos = transform.position;
   Near::Math::Vector3 targetScale = transform.scale;
   if(auto p = player.lock()){
-    targetPos = -p->transform.position;
+    targetPos = p->transform.position;
     targetScale = Near::Math::Vector3(1 + std::max((100 - p->getSize()) / 50.0f, 0.0f));
   }
   float t = std::min(1.0f, deltaTime / 200.0f);
@@ -15,13 +15,13 @@ void PlayerCamera::update(float deltaTime){
 
 void PlayerCamera::draw(){
   Near::Math::Vector3 screenCenter(Near::renderer()->getWidth() / 2.0f, Near::renderer()->getHeight() / 2.0f, 0);
-  Near::renderer2D()->setTransform(Near::Math::Matrix::CreateTranslation(transform.position) * Near::Math::Matrix::CreateScale(transform.scale) * Near::Math::Matrix::CreateTranslation(screenCenter));
+  Near::renderer2D()->setTransform(Near::Math::Matrix::CreateTranslation(-transform.position) * Near::Math::Matrix::CreateScale(transform.scale) * Near::Math::Matrix::CreateTranslation(screenCenter));
 }
 
 void PlayerCamera::setFollow(std::weak_ptr<Player> player){
   this->player = player;
   if(auto p = player.lock()){
-    transform.position = -p->transform.position;
+    transform.position = p->transform.position;
   }
 }
 
